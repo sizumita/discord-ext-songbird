dcefb01f56d24dfb053dd3346a6ec06af81ace4b
sizumita <github@smdr.io>
2025-12-31 22:39:11 +0900

    ✨: Add logging configuration and update Python version requirement to 3.13+

--- examples/basic.py
+++ examples/basic.py
@@ -1,8 +1,12 @@
+import logging
 import os
 
 import discord
 from discord.ext import songbird
 
+FORMAT = '%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s'
+logging.basicConfig(format=FORMAT)
+logging.getLogger().setLevel(logging.DEBUG)
 client = discord.Client(intents=discord.Intents.default())
 
 
