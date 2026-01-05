e99cc427751fc8ea21f2788334e3d1435393c883
sizumita <github@smdr.io>
2026-01-05 11:31:11 +0900

    🐛: __init__.pyi

--- py-src/discord/ext/songbird/__init__.pyi
+++ py-src/discord/ext/songbird/__init__.pyi
@@ -4,5 +4,7 @@ from . import native
 from .client import SongbirdClient as SongbirdClient
 from .native import player as player
 from .native import receive as receive
+from .native.player import InputBase, Queue, Track, TrackHandle
+from .native.player.input import AudioInput, RawPCMInput, StreamInput, SupportedCodec
 
 __all__: tuple[str]
